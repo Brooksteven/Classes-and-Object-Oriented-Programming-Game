588d6e160ac4c4c74dbb4c0256fe98cf5ec745c7
Brooklynn Stevenson <brooksteven580@gmail.com>
2025-03-21 15:29:11 -0400

    updated files

---
+++ index.js
@@ -0,0 +1,14 @@
+//WE ARE CREATING A GAME!!
+
+//Step 1 - Look up documentation to build understanding on:
+//Object-Oriented Adventure
+//Nested Arrays and Objects
+//Combining objects, arrays, and function
+//Create a class to define the blueprint for creating objects
+//How to add a method to a class
+//How to set properties on an instance of a class
+//Make an instance of each class customizable 
+//Create methods to alter the properties of an instance
+//Develop a class that inherits attributes from a "parent" class.
+//Create static properties for a class.
+//Create a "factory."
