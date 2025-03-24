3b25b55093a1fc7f33283dbb0114e34da2950fed
Brooklynn Stevenson <brooksteven580@gmail.com>
2025-03-24 03:19:41 -0400

    update

--- index.js
+++ index.js
@@ -60,24 +60,24 @@
 
 
 
-const adventurer = {
-    name: "Robin",
-    health: 10,
-    inventory: ["sword", "potion", "artifact"],
-    companion: {
-            name: "Frank",
-            type: "Flea",
-            inventory: ["small hat", "sunglasses"]
-        },
-    roll (mod = 0) {
-    const result = Math.floor(Math.random() * 20) + 1 + mod;
-    console.log(`${this.name} rolled a ${result}.`)
-    }
-    }
+// const adventurer = {
+//     name: "Robin",
+//     health: 10,
+//     inventory: ["sword", "potion", "artifact"],
+//     companion: {
+//             name: "Frank",
+//             type: "Flea",
+//             inventory: ["small hat", "sunglasses"]
+//         },
+//     roll (mod = 0) {
+//     const result = Math.floor(Math.random() * 20) + 1 + mod;
+//     console.log(`${this.name} rolled a ${result}.`)
+//     }
+//     }
     
-    adventurer.roll()
-    adventurer.roll()
-    adventurer.roll()
+//     adventurer.roll()
+//     adventurer.roll()
+//     adventurer.roll()
 
 //PART 2: CLASS FANTASY
 //Not sure how companions can give it a try
@@ -87,68 +87,163 @@ const adventurer = {
 //including a constructor function that allows us to create 
 //new characters with whatever name we would like:
 //this is the blueprint on how we build more characters. This is not the Character itself
-class Character {
-    constructor (name) {
-      this.name = name;
-      this.health = 100;
-      this.inventory = [];
-    }
-    roll (mod = 0) {
-        const result = Math.floor(Math.random()*20) + 1 + mod;
-        console.log(`${this.name} rolled a ${result}.`)
-        return result
-      }
-  }
+// class Character {
+//     constructor (name) {
+//       this.name = name;
+//       this.health = 100;
+//       this.inventory = [];
+//     }
+//     roll (mod = 0) {
+//         const result = Math.floor(Math.random()*20) + 1 + mod;
+//         console.log(`${this.name} rolled a ${result}.`)
+//         return result
+//       }
+//   }
 
 
 //Now, we can re-create Robin using the Character class
-const robin = new Character("Robin");
-robin.inventory = ["sword", "potion", "artifact"];
-robin.companion = new Character("Leo");
-robin.companion.type = "Cat";
-robin.companion.companion = new Character("Frank");
-robin.companion.companion.type = "Flea";
-robin.companion.companion.inventory = ["small hat", "sunglasses"];
+// const robin = new Character("Robin");
+// robin.inventory = ["sword", "potion", "artifact"];
+// robin.companion = new Character("Leo");
+// robin.companion.type = "Cat";
+// robin.companion.companion = new Character("Frank");
+// robin.companion.companion.type = "Flea";
+// robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
 
 //PART 3 - CLASS FEATURES
 
-class Adventurer extends Character {
-    constructor (name, role, backpack, water) {
-      super(name);
-      // Adventurers have specialized roles.
-      this.role = role;
-      this.backpack = backpack;
-      this.water = water
-      // Every adventurer starts with a bed and 50 gold coins.
-      this.inventory.push("bedroll", "50 gold coins");
-    }
-    // Adventurers have the ability to scout ahead of them.
-    scout () {
-      console.log(`${this.name} is scouting ahead...`);
-      super.roll();
-    }
-    run () {
-        console.log(`${this.name} is running ahead...`)
-        super.roll()
-    }
-  }
+// class Adventurer extends Character {
+//     constructor (name, role) {
+//       super(name);
+//       // Adventurers have specialized roles.
+//       this.role = role;
+//       // Every adventurer starts with a bed and 50 gold coins.
+//       this.inventory.push("bedroll", "50 gold coins");
+//     }
+//     // Adventurers have the ability to scout ahead of them.
+//     scout () {
+//       console.log(this.name + "is scouting ahead...");
+//       super.roll();
+//     }
+//     run () {
+//         console.log(this.name + "is running ahead...")
+//         super.roll()
+//     }
+//   }
 
  // Next, create a Companion class with
 //properties and methods specific to the companions.
-class companions extends Character{
-    constructor(name, role){
-    this.name = name,
-    this.role = role,
-    }
-}
+// class companions extends Character{
+//     constructor(name, role){
+//     this.name = name,
+//     this.role = role,
+//     }
+// }
 
-const robin = new Character("Robin");
-robin.inventory = ["sword", "potion", "artifact"];
-robin.companion = new Character("Leo");
-robin.companion.type = "Cat";
-robin.companion.companion = new Character("Frank");
-robin.companion.companion.type = "Flea";
-robin.companion.companion.inventory = ["small hat", "sunglasses"];
+// const robin = new Character("Robin");
+// robin.inventory = ["sword", "potion", "artifact"];
+// robin.companion = new Character("Leo");
+// robin.companion.type = "Cat";
+// robin.companion.companion = new Character("Frank");
+// robin.companion.companion.type = "Flea";
+// robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
+
+//PART 4 - CLASS UNIFORMS 
+
+//USE STATIC PROPERTIES AND METHODS TO CREATE 
+//UNIFORM ATTRIBUTES FOR THE CLASS ITSELF, DON'T DO INSTANCES OF THE CLASS
+// class Character {
+//    static MAX_HEALTH = 100
+
+// constructor(name){
+//     this.name = name,
+//     this.health = Character.MAX_HEALTH,
+//     this.inventory = []
+// }
+
+// roll(mod = 0){
+//     const result = Math.floor(Math.random() * 20) + 1 + mod
+//     console.log(this.name + " rolled a " + result)
+//     return result
+// }
+// }
+
+
+// class Adventurer extends Character {
+//     static ROLES = ["Fighter", "Healer", "Wizard"]
+    
+//     constructor(name, role) {
+//         super(name),
+//         this.role = role,
+//         this.inventory.push("bedroll"),
+//         this.inventory.push("50 gold coins")
+//     }
+
+//     scout() {
+//         console.log(this.name + " is scouting ahead...")
+//         this.roll(),
+//     }
+// }
+
+//PART 5 - Gather Your Party
+
+// class AdventurerFactory {  
+//     constructor (role) {
+//       this.role = role;
+//       this.adventurers = [];
+//     }
+//     generate (name) {
+//       const newAdventurer = new Adventurer(name, this.role);
+//       this.adventurers.push(newAdventurer);
+//     }
+//     findByIndex (index) {
+//       return this.adventurers[index];
+//     }
+//     findByName (name) {
+//       return this.adventurers.find((a) => a.name === name);
+//     }
+//   }
+  
+//   const healers = new AdventurerFactory("Healer");
+//   const robin = healers.generate("Robin");
+
+//PART 6 - Developing Skills
+
+//   class Adventurer extends Character {
+//     constructor (name, role) {
+//       super(name);
+//       // Adventurers have specialized roles.
+//       this.role = role;
+//       // Every adventurer starts with a bed and 50 gold coins.
+//       this.inventory.push("bedroll", "50 gold coins");
+//     }
+//     // Adventurers have the ability to scout ahead of them.
+//     scout () {
+//       console.log(this.name + "is scouting ahead...");
+//       super.roll();
+//     }
+
+//     duel(person){
+//         if (this.health > 50 && person.health > 50) {
+//             const theRoll = this.roll();
+//             const theirRoll = person.roll()
+//       }
+
+//       if (theRoll > theirRoll) {
+//         person.health = person.health - 1
+//       }else if (person.roll > theRoll){
+//         this.health = this.health - 1
+//       }
+
+//       let winner
+//       if (this.health > 50) {
+//         winner = this.name
+//       } else {
+//         winner = person.name
+//       }
+
+//       console.log("Congrats to " + winner)
+//     }
 
